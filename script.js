5a2d33a7c910810814436a81db4d06018cfbdcb6
mariaeduardaporto <maria.porto.tiburcio@escola.pr.gov.br>
2026-09-25 08:21:36 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const botoeCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventlistener("click", curtir);
+function curtir(){
+    const contador = botaoCurtir.querySelector("span");
+    if(curtiu === false){
+        contador.textContent++;
+        curtiu = true;
+    } else{
+        contador.textContent--;
+        curtiu = false;
+    }
 
+    }
+});
